6a0452be0e878dcaf8c47250ea906b2cc244386b
ahmed adel <ahmedadel123422@gmail.com>
2024-12-16 23:14:00 +0200

    commit

--- lib/features/intro/home/presentation/pages/home_screen.dart
+++ lib/features/intro/home/presentation/pages/home_screen.dart
@@ -1,17 +1,76 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:developer';
+
+import 'package:bookia_store/core/constants/app_assets.dart';
+import 'package:bookia_store/core/utils/colors.dart';
+import 'package:bookia_store/core/utils/text_style.dart';
+import 'package:bookia_store/features/intro/home/presentation/widgets/banner.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Icon(
-          Icons.abc,
-          size: 400,
+      appBar: AppBar(
+        title: SvgPicture.asset(
+          AppAssets.logoSvg,
+          width: 100,
+        ),
+        actions: [
+          InkWell(
+            child: SvgPicture.asset(AppAssets.notificationSvg),
+            onTap: () {},
+          ),
+          Gap(16),
+          GestureDetector(
+            child: SvgPicture.asset(AppAssets.searchSvg),
+            onTap: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BannerWidget(),
+            Gap(30),
+            Text(
+              "Popular Books",
+              style: getFont24TextStyle(),
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: AppColors.borderColor,
+                  child: Column(
+                    children: [
+                      Image.asset(AppAssets.blackHeartPng),
+                    ],
+                  ),
+                );
+              },
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/features/intro/home/presentation/widgets/banner.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: annotate_overrides, prefer_const_constructors
+
+import 'package:bookia_store/core/constants/app_assets.dart';
+import 'package:bookia_store/core/utils/colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class BannerWidget extends StatefulWidget {
+  const BannerWidget({super.key});
+
+  @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  @override
+  int currentIndex = 0;
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Gap(25),
+        CarouselSlider.builder(
+          itemCount: 3,
+          itemBuilder:
+              (BuildContext context, int itemIndex, int pageViewIndex) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                AppAssets.blackHeartPng,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            );
+          },
+          options: CarouselOptions(
+            height: 150,
+            viewportFraction: 0.85,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            enlargeFactor: 0.2,
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (index, reason) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+          ),
+        ),
+        Gap(14),
+        SmoothPageIndicator(
+          effect: ExpandingDotsEffect(
+            spacing: 10.0,
+            dotHeight: 7,
+            dotWidth: 7,
+            strokeWidth: 1.5,
+            dotColor: Colors.grey,
+            expansionFactor: 5,
+            activeDotColor: AppColors.primaryColor,
+          ),
+          controller:
+              PageController(initialPage: currentIndex), // PageController
+          count: 3,
+          onDotClicked: (index) {},
+        ),
+      ],
+    );
+  }
+}
