7b38ba4d58aa816be595584d848b783419b32a4f
ahmed adel <ahmedadel123422@gmail.com>
2024-12-25 22:42:09 +0200

    commit

--- lib/features/intro/home/presentation/pages/book_details_screen.dart
+++ lib/features/intro/home/presentation/pages/book_details_screen.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bookia_store/core/constants/app_assets.dart';
+import 'package:bookia_store/core/utils/colors.dart';
+import 'package:bookia_store/core/utils/text_style.dart';
+import 'package:bookia_store/features/intro/home/data/models/get_new_arrivals_response/product.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class BookDetails extends StatefulWidget {
-  const BookDetails({super.key});
+  BookDetails({super.key, required this.product});
+  final Product product;
 
   @override
   State<BookDetails> createState() => _BookDetailsState();
@@ -13,9 +19,71 @@ class _BookDetailsState extends State<BookDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [],
+      appBar: AppBar(
+        toolbarHeight: 60,
+        surfaceTintColor: AppColors.whiteColor,
+        backgroundColor: Colors.white, // Matches the background
+        elevation: 0, // Removes the shadow
+        leading: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context); // Navigate back
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 10),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white, // Background color
+                  borderRadius: BorderRadius.circular(20.5), // Circular border
+                  border: Border.all(
+                    color: AppColors.borderColor, // Border color
+                    width: 2.0, // Border width
+                  ),
+                ),
+                child: Icon(
+                  Icons.arrow_back_ios, // Back arrow icon
+                  color: Colors.black, // Icon color
+                  size: 25, // Icon size
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(AppAssets.bookMarkPng),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+              widget.product.image ?? " ",
+              height: 271,
+            ),
+            Text(
+              "Black Heart",
+              style: getFont30TextStyle(),
+            ),
+            Text(
+              "Broché",
+              style: getFont16TextStyle(
+                color: AppColors.primaryColor,
+              ),
+            ),
+            Text(
+              widget.product.description ?? "There is No description",
+              style: getFont14TextStyle(
+                color: AppColors.darkColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/intro/home/presentation/widgets/books.dart
+++ lib/features/intro/home/presentation/widgets/books.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:bookia_store/core/functions/navigation.dart';
 import 'package:bookia_store/core/utils/colors.dart';
 import 'package:bookia_store/core/utils/text_style.dart';
 import 'package:bookia_store/core/widgets/custom_button.dart';
 import 'package:bookia_store/features/intro/home/presentation/bloc/home_bloc.dart';
+import 'package:bookia_store/features/intro/home/presentation/pages/book_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
@@ -61,81 +63,96 @@ class _BooksState extends State<Books> {
                         color: AppColors.secondaryColor,
                         borderRadius: BorderRadius.circular(11.0),
                       ),
-                      child: Column(
-                        children: [
-                          Stack(
-                            children: [
-                              Expanded(
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image.network(
-                                    "${booksList[index].image}",
-                                    width: double.infinity,
-                                    fit: BoxFit.cover,
+                      child: GestureDetector(
+                        onTap: () {
+                          pushTo(
+                              context,
+                              BookDetails(
+                                product: booksList[index],
+                              ));
+                        },
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Expanded(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.network(
+                                      height: 200,
+                                      "${booksList[index].image}",
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Positioned(
-                                top: 0,
-                                right: 0,
-                                child: Transform.rotate(
-                                  angle: 0,
-                                  alignment: Alignment.centerRight,
-                                  child: Container(
-                                    width: 50,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(4),
-                                      color: AppColors.discountColor,
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        "${booksList[index].discount}%",
-                                        style: getFont14TextStyle(
-                                          color: AppColors.whiteColor,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.bold,
+                                Positioned(
+                                  top: 0,
+                                  right: 0,
+                                  child: Transform.rotate(
+                                    angle: 0,
+                                    alignment: Alignment.centerRight,
+                                    child: Container(
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                        color: AppColors.discountColor,
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "${booksList[index].discount}%",
+                                          style: getFont14TextStyle(
+                                            color: AppColors.whiteColor,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Gap(20),
-                              Text(
-                                booksList[index].name ?? " ",
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                // bookList.,
-                                style: getFont16TextStyle(),
-                              ),
-                              Gap(3),
-                              Row(
-                                children: [
-                                  Text(
-                                    "${booksList[index].priceAfterDiscount} Eg",
-                                    style: getFont16TextStyle(),
-                                  ),
-                                  Spacer(),
-                                  CustomButton(
-                                    radius: 4,
-                                    text: "Buy",
-                                    fontsize: 14,
-                                    textColor: AppColors.whiteColor,
-                                    color: AppColors.darkColor,
-                                    onpressed: () {},
-                                    width: 73,
-                                    height: 30,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  booksList[index].name ?? " ",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  // bookList.,
+                                  style: getFont16TextStyle(),
+                                ),
+                                Gap(3),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "${booksList[index].priceAfterDiscount} Eg",
+                                      style: getFont16TextStyle(),
+                                    ),
+                                    Spacer(),
+                                    CustomButton(
+                                      radius: 4,
+                                      text: "Buy",
+                                      fontsize: 14,
+                                      textColor: AppColors.whiteColor,
+                                      color: AppColors.darkColor,
+                                      onpressed: () {
+                                        pushTo(
+                                            context,
+                                            BookDetails(
+                                              product: booksList[index],
+                                            ));
+                                      },
+                                      width: 73,
+                                      height: 30,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
